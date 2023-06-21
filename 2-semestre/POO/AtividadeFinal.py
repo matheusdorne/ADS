9c8f3f3f6bf9d6202a7eb19a461ef3af14ce44b7
theusdorne <matheus.dorne@gmail.com>
2023-06-21 18:38:20 -0300

    ajuste

--- 2-semestre/POO/AtividadeFinal.py
+++ 2-semestre/POO/AtividadeFinal.py
@@ -78,7 +78,7 @@ Escolha: '''))
         elif escolha == 5:
             relatorio_conferencia()
         elif escolha == 6:
-            gerar_arquivo()
+            gerar_arquivo_json()
         elif escolha == 7:
             ler_arquivo()
         elif escolha == 8:
@@ -103,6 +103,8 @@ def testaInt(variavel):
     except ValueError:
         print("\nDigite uma valor válido!\n")
         return False
+
+
 def converteParaInt(variavel):
     try:
         valor = int(variavel)
@@ -155,14 +157,15 @@ def listar_profissionais():
         contador += 1
     while True:
         escolha = input("Escolha: ")
-        escolhaint , boolean = converteParaInt(escolha)
+        escolhaint, boolean = converteParaInt(escolha)
         if boolean:
-            if  0 <= escolhaint <= contador:
+            if 0 <= escolhaint <= contador:
                 return l_profissionais[escolhaint]
             else:
                 print("Opção inválida")
         else:
             print("Digite um valor de 0 a ", contador, "")
+
 
 def listar_visitantes():
     contador = 0
@@ -171,7 +174,7 @@ def listar_visitantes():
         contador += 1
     while True:
         escolha = input("Escolha: ")
-        escolhaint , boolean = converteParaInt(escolha)
+        escolhaint, boolean = converteParaInt(escolha)
         if boolean:
             if 0 <= escolhaint <= contador:
                 return l_visitantes[escolhaint]
@@ -180,33 +183,49 @@ def listar_visitantes():
         else:
             print("Digite um valor de 0 a ", contador, "")
 
+
 def registrar_visita():
     profissional = listar_profissionais()
     visitante = listar_visitantes()
-    data_entrada = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    data_entrada = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     if visitante in dict_visitas:
-        dict_visitas[visitante] = {"nome_profissional": profissional.getNome(), "data_entrada": data_entrada, "Sala:": profissional.getSala()}
+        dict_visitas[visitante].append({"nome_profissional": profissional.getNome(), "data_entrada": data_entrada,
+                                        "Sala:": profissional.getSala()})
     else:
-        dict_visitas[visitante] = {}
-        dict_visitas[visitante] = {"nome_profissional": profissional.getNome(), "data_entrada": data_entrada, "Sala:": profissional.getSala()}
+        dict_visitas[visitante] = []
+        dict_visitas[visitante].append({"nome_profissional": profissional.getNome(), "data_entrada": data_entrada,
+                                        "Sala:": profissional.getSala()})
     print(dict_visitas)
 
+
 def relatorio_conferencia():
-    pass
+    profissional = listar_profissionais()
+    for i in dict_visitas:
+        for j in dict_visitas[i]:
+            if j["nome_profissional"] == profissional.getNome():
+                print(i.retornaNome(), j["data_entrada"], j["Sala:"])
 
 
-def gerar_arquivo():
-    pass
+def gerar_arquivo_json():
+    data_atual = datetime.datetime.now().strftime("%Y-%m-%d")
+    nome_arquivo = f"registros_{data_atual}.json"
+
+    with open(nome_arquivo, "w") as arquivo:
+        json.dump(dict_visitas, arquivo)
+
+    print("Arquivo de registros do dia gerado com sucesso!")
+
 
 
 def ler_arquivo():
     pass
 
+
 l_profissionais = []
-l_profissionais.append(Profissional("MATHEUS","ODONTOLOGIA","1"))
+l_profissionais.append(Profissional("MATHEUS", "ODONTOLOGIA", "1"))
 l_visitantes = []
-l_visitantes.append(Visitante("JOAO","123456789"))
+l_visitantes.append(Visitante("JOAO", "123456789"))
 dict_visitas = {}
 print(l_profissionais[0])
 menu()
